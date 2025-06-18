5ed200eeebb9d58af9736f3e812c71621cdf567c
Renato Nunes Junior <renatonj0489@gmail.com>
2025-06-18 14:34:50 -0300

    atualização

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 window.addEventListener("load", function () {
-  alert("Olá, eu sou a rocha de estimação.");
+  const fala = document.getElementById("balao-fala");
+  const texto = document.getElementById("texto-fala");
+
+  fala.classList.remove("escondido");
+  texto.innerText = "Olá, eu sou a rocha de estimação.";
+
+  setTimeout(() => {
+    texto.innerText = "Qual o seu nome?";
+    document.getElementById("input-nome").classList.remove("escondido");
+  }, 3000);
 });
 
-function touchRock() {
-    const userName = prompt("Qual o seu nome?", "Digite seu nome aqui.")
+function enviarNome() {
+  const nome = document.getElementById("nome").value.trim();
+  const texto = document.getElementById("texto-fala");
 
-    if (userName) {
-        alert("Prazer em conhecer você, " + userName + ".")
-        document.getElementById("rocking").src = "./img/rock_happy.png"
-    }
+  if (nome) {
+    texto.innerText = `Prazer em conhecer você, ${nome}!`;
+    document.getElementById("input-nome").classList.add("escondido");
+    document.getElementById("rocking").src = "./img/rock_happy.png";
+  } else {
+    alert("Por favor, digite seu nome.");
+  }
 }
