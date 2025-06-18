9604877c96887e3d2f5c4c96a57ab85e06d540b4
Renato Nunes Junior <renatonj0489@gmail.com>
2025-06-18 15:33:06 -0300

    responsivo

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 window.addEventListener("load", function () {
   const fala = document.getElementById("balao-fala");
   const texto = document.getElementById("texto-fala");
+  const inputNome = document.getElementById("input-nome");
 
+  // Mostrar balão
   fala.classList.remove("escondido");
   texto.innerText = "Olá, eu sou a rocha de estimação.";
 
+  // Esconde o campo de nome durante a fala inicial
+  inputNome.classList.add("escondido");
+
   setTimeout(() => {
     texto.innerText = "Qual o seu nome?";
-    document.getElementById("input-nome").classList.remove("escondido");
+    inputNome.classList.remove("escondido");
   }, 3000);
 });
 
 function enviarNome() {
   const nome = document.getElementById("nome").value.trim();
   const texto = document.getElementById("texto-fala");
+  const inputNome = document.getElementById("input-nome");
 
   if (nome) {
     texto.innerText = `Prazer em conhecer você, ${nome}!`;
-    document.getElementById("input-nome").classList.add("escondido");
+    inputNome.classList.add("escondido");
     document.getElementById("rocking").src = "./img/rock_happy.png";
   } else {
     alert("Por favor, digite seu nome.");
